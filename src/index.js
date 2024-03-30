83c0ba2e7083773b0f497ac0562fb1389b48c159
zanwhip <hungnv.21it@vku.udn.vn>
2024-03-31 01:20:54 +0700

    aaa

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const e = require('express');
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}
+ 
+))
+app.use(express.json())
 
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // template engine
 app.engine('hbs', handlebars.engine({ defaultLayout: 'main', extname : `.hbs` }));
@@ -26,4 +33,14 @@ app.get('/news', (req, res) => {
   res.render(`news`)
 })
 
+
+
+app.get('/search', (req, res) => {
+  res.render(`search`)
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send(``)
+})
 app.listen(port, () => console.log(`Listen on http://localhost:${port}`))
